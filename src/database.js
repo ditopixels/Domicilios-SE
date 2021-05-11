01626008fe6a46294dec3f079dcb97a349bad61a
Juan Diego Durango <juandiego90123@gmail.com>
2021-05-11 17:56:48 -0500

    Despliegue app 2.0

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb://localhost/domicilios', {
+mongoose.connect('mongodb+srv://juandiego:tests123@cluster0.lxmiy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
         useUnifiedTopology: true,
         useNewUrlParser: true,
         useFindAndModify: false,
