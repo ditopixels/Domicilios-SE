4bc7afa70b2706241e62f53df8eaec8c742ea992
Juan Diego Durango <juandiego90123@gmail.com>
2021-05-11 18:13:15 -0500

    Despliegue app 3.1

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,35 +1,38 @@
 import Products from '../models/Products.js'
 
 export const renderHome = async(req, res) => {
+    try {
+        const productCategories = []
 
-    const productCategories = []
-
-    const data = {
-        categories: [],
-    }
-    const products = await Products.find().sort({ date: -1 })
-
-    products.forEach(product => {
-        const { title, description, image, price, categorie, _id } = product
-
-        if (!productCategories.includes(categorie)) {
-            productCategories.push(categorie)
-            return data.categories.push({
-                name: categorie,
-                products: [{
-                    title,
-                    description,
-                    image,
-                    price,
-                    _id
-                }]
-            })
+        const data = {
+            categories: [],
         }
-        data.categories[productCategories.indexOf(categorie)].products.push({ title, description, image, price, _id })
+        const products = await Products.find().sort({ date: -1 })
+
+        products.forEach(product => {
+            const { title, description, image, price, categorie, _id } = product
+
+            if (!productCategories.includes(categorie)) {
+                productCategories.push(categorie)
+                return data.categories.push({
+                    name: categorie,
+                    products: [{
+                        title,
+                        description,
+                        image,
+                        price,
+                        _id
+                    }]
+                })
+            }
+            data.categories[productCategories.indexOf(categorie)].products.push({ title, description, image, price, _id })
 
 
-    })
-    data.msg = 'Pedido realizado, pronto nos estaremos comunicando contigo'
-    console.log(data)
-    res.render('home', data)
+        })
+        data.msg = 'Pedido realizado, pronto nos estaremos comunicando contigo'
+        console.log(data)
+        res.render('home', data)
+    } catch (e) {
+        console.log('error: ', e)
+    }
 }
